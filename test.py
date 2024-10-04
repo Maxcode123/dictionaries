a16c5ea0697bb3b767b35dda92679894f946dbf1
Maximos Nikiforakis <nikiforos@live.co.uk>
2024-10-04 18:09:01 +0300

    Adds test python code

---
+++ test.py
@@ -0,0 +1,13 @@
+from dictionaries import SSDict
+
+d = SSDict()
+
+d.__setitem__(1, 11)
+
+print("\nSet item successfully!\n")
+
+print(f"dictionary length: {d.__len__()}")
+
+print("\n\n--------------------------------------")
+print("Program ran successfully!")
+print("--------------------------------------\n\n")
