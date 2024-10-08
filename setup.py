c477fd80e84adf29d8ca311cd0b8ed82039d3c84
Maximos Nikiforakis <nikiforos@live.co.uk>
2024-10-08 08:34:10 +0300

    Adds files to extension sources

--- setup.py
+++ setup.py
@@ -1,3 +1,11 @@
 from setuptools import Extension, setup
 
-setup(ext_modules=[Extension("dictionaries", ["dictionaries.c"])])
+setup(
+    ext_modules=[
+        Extension(
+            "dictionaries",
+            sources=["src/ssdict/ssdict.c", "src/ssdict/node.c"],
+            include_dirs=["src/ssdict"],
+        )
+    ]
+)
