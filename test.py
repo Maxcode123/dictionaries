4adcdf8cc6b66aba48aa11103f0eee63108efc20
Maximos Nikiforakis <nikiforos@live.co.uk>
2024-10-09 15:57:33 +0300

    Adds python test cases

--- test.py
+++ test.py
@@ -1,13 +1,56 @@
+import unittest
+
+from unittest_extensions import TestCase, args
+
 from dictionaries import SSDict
 
-d = SSDict()
 
-d.__setitem__(1, 11)
+class TestSSDictSubscript(TestCase):
+    def subject(self, key):
+        return self.dict()[key]
 
-print("\nSet item successfully!\n")
+    def dict(self) -> SSDict:
+        return self._dict
 
-print(f"dictionary length: {d.__len__()}")
+    def setUp(self):
+        self._dict = SSDict()
 
-print("\n\n--------------------------------------")
-print("Program ran successfully!")
-print("--------------------------------------\n\n")
+    @args({"key": 2})
+    def test_unset_item(self):
+        self.assertResultRaises(KeyError)
+
+    @args({"key": 1})
+    def test_set_item(self):
+        self.dict().__setitem__(1, 11)
+        self.assertResult(11)
+
+    def test_unset_unhashable_item(self): ...
+
+
+class TestSSDictAssSubscript(TestCase):
+    pass
+
+
+class TestSSDictLength(TestCase):
+    pass
+
+
+if __name__ == "__main__":
+    unittest.main()
+
+
+# d = SSDict()
+
+# d.__setitem__(2, 20)
+
+# d[2] = 10
+
+# print("\nSet item successfully!\n")
+
+# print(f"\nd[2] = {d.__getitem__(2)}\n")
+
+# print(f"dictionary length: {d.__len__()}")
+
+# print("\n\n--------------------------------------")
+# print("Program ran successfully!")
+# print("--------------------------------------\n\n")
