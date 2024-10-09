10ec604c6355294376df96571fc44fbabf0bb02a
Maximos Nikiforakis <nikiforos@live.co.uk>
2024-10-09 16:09:44 +0300

    Adjusts example.py

--- example.py
+++ example.py
@@ -1,19 +1,6 @@
 from dictionaries import SSDict
 
-
 d = SSDict()
-
-# d.__setitem__(2, 2)
 
 d[1] = 2
 d[3] = 4
-
-print("\nSet item successfully!\n")
-
-# print(f"\nd[2] = {d[2]}\n")
-
-print(f"dictionary length: {d.__len__()}")
-
-print("\n\n--------------------------------------")
-print("Program ran successfully!")
-print("--------------------------------------\n\n")
